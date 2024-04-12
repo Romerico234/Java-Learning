78c43f0c0ba401558226b3b1dde7f19661bdf97c
Romerico <mrh.jrd.234@gmail.com>
2024-04-12 10:13:56 -0400

    updated spacing/formatting for assign 6

--- COSC-336/Assignment 6/BST.java
+++ COSC-336/Assignment 6/BST.java
@@ -22,7 +22,7 @@ class BinarySearchTree {
 
 	// A utility function to insert
 	// a new node with given key in BST
-	Node insert(Node node, int key) { 
+	Node insert(Node node, int key) {
 		// If the tree is empty, return a new node
 		if (node == null) {
 			node = new Node(key);
@@ -30,7 +30,7 @@ class BinarySearchTree {
 		}
 
 		// Otherwise, recur down the tree
-		if (key <= node.key) //Adds the key to the left subtree if the key is less than or EQUAL to the root
+		if (key <= node.key) // Adds the key to the left subtree if the key is less than or EQUAL to the root
 			node.left = insert(node.left, key);
 		else
 			node.right = insert(node.right, key);
@@ -72,46 +72,46 @@ class BinarySearchTree {
 		x.right = t;
 		return x;
 	}
-	
 
 	void printPreorder(Node node) {
 		if (node == null)
 			return;
+		// add commas between each pair but exclude the last pair
 		System.out.print("(" + node.key + "," + size(node) + ")");
 		printPreorder(node.left);
 		printPreorder(node.right);
 	}
 
 	public static void main(String[] args) {
-		int[] data1 = {7, 10, 3, 9, 13, 11};
+		int[] data1 = { 7, 10, 3, 9, 13, 11 };
 		int[] data2 = fileReader("input-6-1.txt");
 		int[] data3 = fileReader("input-6-2.txt");
-		
+
 		BinarySearchTree A = insertToBST(data1);
 		BinarySearchTree B = insertToBST(data2);
 		BinarySearchTree C = insertToBST(data3);
-		
-		// System.out.print("Preorder traversal of A is: ");
-		// A.printPreorder(A.root);
-		// System.out.println();
-		// //Left rotating the tree
-		// A.root = A.leftRotate(A.root);
-		// System.out.print("Preorder traversal of A after left rotation is: ");
-		// A.printPreorder(A.root);
-		// System.out.println();
-		
-		// System.out.println("---------------------------------");
-		
-		System.out.print("Preorder traversal of B is: ");
-		B.printPreorder(B.root);
-		System.out.println(); 
-		B.root = B.leftRotate(B.root);
-		System.out.print("Preorder traversal of B after left rotation is: ");
-		B.printPreorder(B.root);
+
+		System.out.print("Preorder traversal of A is: ");
+		A.printPreorder(A.root);
 		System.out.println();
-		
+		// Left rotating the tree
+		A.root = A.leftRotate(A.root);
+		System.out.print("Preorder traversal of A after left rotation is: ");
+		A.printPreorder(A.root);
+		System.out.println();
+
+		System.out.println("---------------------------------");
+
+		// System.out.print("Preorder traversal of B is: ");
+		// B.printPreorder(B.root);
+		// System.out.println();
+		// B.root = B.leftRotate(B.root);
+		// System.out.print("Preorder traversal of B after left rotation is: ");
+		// B.printPreorder(B.root);
+		// System.out.println();
+
 		// System.out.println("---------------------------------");
-		
+
 		// // System.out.print("Preorder traversal of C is: ");
 		// // C.printPreorder(C.root);
 		// // System.out.println();
@@ -122,25 +122,25 @@ class BinarySearchTree {
 	}
 
 	public static int[] fileReader(String fileName) {
-        try {
-            File file = new File(fileName);
-            Scanner fileReader = new Scanner(file);
-            int size = 0;
-            if (fileReader.hasNextInt())
-                size = fileReader.nextInt();
-            int[] list = new int[size];
-            int i = 0;
-            while (fileReader.hasNextInt())
-                list[i++] = fileReader.nextInt();
+		try {
+			File file = new File(fileName);
+			Scanner fileReader = new Scanner(file);
+			int size = 0;
+			if (fileReader.hasNextInt())
+				size = fileReader.nextInt();
+			int[] list = new int[size];
+			int i = 0;
+			while (fileReader.hasNextInt())
+				list[i++] = fileReader.nextInt();
 			fileReader.close();
-            return list;
-        } catch (FileNotFoundException e) {
-            System.out.println("File not found: " + e.getMessage());
-        }
-        return null;
-    }
+			return list;
+		} catch (FileNotFoundException e) {
+			System.out.println("File not found: " + e.getMessage());
+		}
+		return null;
+	}
 
-	public static BinarySearchTree insertToBST(int[] list){
+	public static BinarySearchTree insertToBST(int[] list) {
 		BinarySearchTree tree = new BinarySearchTree();
 		for (int i = 0; i < list.length; i++) {
 			tree.root = tree.insert(tree.root, list[i]);
