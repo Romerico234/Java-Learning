f0a963976d67567e65294470adfa4a6a1275eae1
Romerico <mrh.jrd.234@gmail.com>
2024-04-25 09:58:42 -0400

    update

--- COSC-336/Assignment 7/Assign7.java
+++ COSC-336/Assignment 7/Assign7.java
@@ -64,16 +64,4 @@ public class Assign7 {
                     g2.addEdge(i, k);
         return g2;
     }
-
-    /*
-    public static Adj_List_Graph createGraphSquared(Adj_List_Graph g) {
-        Adj_List_Graph g2 = new Adj_List_Graph(g);
-        for (int i = 0; i < g.n; i++) 
-            for (int j = 0; j < g.n; j++)
-                for (int k = 0; k < g.n; k++)
-                    if (g.adj.get(i).contains(j) && g.adj.get(j).contains(k))
-                        g2.addEdge(i, k);
-        return g2;
-    }
-     */
 }
